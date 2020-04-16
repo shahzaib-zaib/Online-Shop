7309a836b1df19689c9b230da620b9ae92269833
Shahzaib <shahzaibux@gmail.com>
2020-04-16 14:54:41 +0500

    Add project files.

---
+++ OnlineShop.Database/OSContext.cs
@@ -0,0 +1,19 @@
+﻿using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Database
+{
+    public class OSContext : DbContext
+    {
+        public OSContext() : base("OnlineShop")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ OnlineShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            return View();
+        }
+    }
+}
