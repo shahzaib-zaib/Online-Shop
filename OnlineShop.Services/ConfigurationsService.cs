9c0c6676d2779643742d73c9b4accda8a6af5450
Shahzaib <shahzaibux@gmail.com>
2020-04-25 03:17:32 +0500

    Edited Intance in ConfigurationService

--- OnlineShop.Services/ConfigurationsService.cs
+++ OnlineShop.Services/ConfigurationsService.cs
@@ -10,6 +10,18 @@ namespace OnlineShop.Services
 {
     public class ConfigurationsService
     {
+        public static ConfigurationsService ClassObject { 
+            get {
+                if (privateInMemoryObject == null) privateInMemoryObject = new ConfigurationsService();
+
+                return privateInMemoryObject;
+            } 
+        }
+        private static ConfigurationsService privateInMemoryObject { get; set; }
+
+        private ConfigurationsService()
+        {
+        }
         public Config GetConfig(string key)
         {
             using(var context = new OSContext())
