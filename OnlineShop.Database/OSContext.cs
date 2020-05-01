82ebc9734562f3a8b844f25b1543a90b3e8ccad3
Shahzaib <shahzaibux@gmail.com>
2020-05-01 14:46:51 +0500

    Updated Validation in OSContext

--- OnlineShop.Database/OSContext.cs
+++ OnlineShop.Database/OSContext.cs
@@ -13,6 +13,10 @@ namespace OnlineShop.Database
         public OSContext() : base("OnlineShop")
         {
         }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().Property(p => p.Name).IsRequired().HasMaxLength(50);
+        }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Config> Configurations { get; set; }
