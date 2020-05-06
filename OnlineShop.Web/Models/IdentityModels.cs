db6c5351bf986454e84a5b23120af4939232f26e
Shahzaib <shahzaibux@gmail.com>
2020-05-07 02:11:36 +0500

    Created Login and Register Forms

--- OnlineShop.Web/Models/IdentityModels.cs
+++ OnlineShop.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace OnlineShop.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("OnlineShop", throwIfV1Schema: false)
         {
         }
 
