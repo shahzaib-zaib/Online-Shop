6b78759ac2716f169ab5453af7aa1c8d40ac36b8
Shahzaib <shahzaibux@gmail.com>
2020-05-13 21:28:53 +0500

    Created OrderServices OrderController OrderViewModels and Index Page

---
+++ OnlineShop.Services/OrderServices.cs
@@ -0,0 +1,68 @@
+﻿using OnlineShop.Database;
+using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Services
+{
+    public class OrderServices
+    {
+        #region Singleton
+        public static OrderServices Instance
+        {
+            get
+            {
+                if (instance == null) instance = new OrderServices();
+
+                return instance;
+            }
+        }
+        private static OrderServices instance { get; set; }
+
+        private OrderServices()
+        {
+        }
+        #endregion
+
+        public List<Order> SearchOrders(string userID, string status, int pageNo, int pageSize)
+        {
+            using (var context = new OSContext())
+            {
+                var orders = context.Orders.ToList();
+
+                if (!string.IsNullOrEmpty(userID))
+                {
+                    orders = orders.Where(x => x.UserID.ToLower().Contains(userID.ToLower())).ToList();
+                }
+                if (!string.IsNullOrEmpty(status))
+                {
+                    orders = orders.Where(x => x.Status.ToLower().Contains(status.ToLower())).ToList();
+                }
+                
+                return orders.Skip((pageNo - 1) * pageSize).Take(pageSize).ToList();
+            }
+        }
+
+        public int SearchOrdersCount(string userID, string status)
+        {
+            using (var context = new OSContext())
+            {
+                var orders = context.Orders.ToList();
+
+                if (!string.IsNullOrEmpty(userID))
+                {
+                    orders = orders.Where(x => x.UserID.ToLower().Contains(userID.ToLower())).ToList();
+                }
+                if (!string.IsNullOrEmpty(status))
+                {
+                    orders = orders.Where(x => x.Status.ToLower().Contains(status.ToLower())).ToList();
+                }
+
+                return orders.Count;
+            }
+        }
+    }
+}
---
+++ OnlineShop.Web/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using OnlineShop.Services;
+using OnlineShop.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index(string userID, string status, int? pageNo)
+        {
+            OrdersViewModel model = new OrdersViewModel();
+            model.UserID = userID;
+            pageNo = pageNo.HasValue ? pageNo.Value > 0 ? pageNo.Value : 1 : 1; //Use this or else Down one
+            var pageSize = ConfigurationsService.Instance.PageSize();
+            model.Orders = OrderServices.Instance.SearchOrders(userID, status, pageNo.Value, pageSize);
+
+            var totalRecords = OrderServices.Instance.SearchOrdersCount(userID, status);
+
+            model.Pager = new Pager(totalRecords, pageNo, pageSize);
+            return View();
+        }
+    }
+}
